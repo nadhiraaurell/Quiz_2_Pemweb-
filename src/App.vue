30f817f4c91ebaf2cdaeaa34511d4bca127be13c
nadhiraaurell <nadhiraurelll@gmail.com>
2023-06-06 23:45:37 +0700

    Pengumpulan Quiz 2

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import NavBar2 from './components/NavBar2.vue';
+import NavBar3 from './components/NavBar3.vue';
+
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+    <NavBar2 />
+    <NavBar3 />
+ </template>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/boostrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import '@lottiefiles/lottie-playerD'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
