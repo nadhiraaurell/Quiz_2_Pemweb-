c5e697fc77159945ce7e8e3a2515e75d36fbc04d
nadhiraaurell <nadhiraurelll@gmail.com>
2023-06-11 22:10:31 +0700

    update code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/boostrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-import '@lottiefiles/lottie-playerD'
+import '@lottiefiles/lottie-player'
 
 
 import { createApp } from 'vue'
